39e0e7ea0fb19944b63d31435e565ea7b0e82da1
Mattia Pietro Martolini <martolinimattiapietro@gmail.com>
2024-02-12 14:20:33 +0100

    Esercizio Finito

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
-const apiKey = '6QUvkwoZbg1G58kySwhsuG2CXhBGgOMdbbX3yXFXCxHMuLkqP7DERqfB';
+/* const apiKey = '6QUvkwoZbg1G58kySwhsuG2CXhBGgOMdbbX3yXFXCxHMuLkqP7DERqfB'; */
+const apiKey = '563492ad6f91700001000001b0ec30909a274fddaf88db2ac6d23e44';
+
 
 function loadData(search){
   fetch(`https://api.pexels.com/v1/search?query=`+search, {
   headers: {
-    Authorization: apiKey
+    Authorization: apiKey,
   }
   })
   .then((response) => response.json())
-  .then((json) => console.log(json))
-  .then(json => {
-    let containerFoto = document.getElementById("container-photos");
-        const photos = json.photos; // Array contenente gli album
+  .then(res => {
+    let cont = document.querySelector(".row");
 
-        // Itera su ogni album ottenuto
-        photos.forEach(img => {
-            // Crea un nuovo div per l'album
-            const albumDiv = document.createElement("div");
-            albumDiv.classList.add("album");
+    cont.innerHTML = res.photos.map((photo) => {
+      return ` <div class='col col-4'> <div class="card mb-4 shadow-sm">
+              <img src='${photo.src.large}' />
 
-            // Crea un'immagine per l'album
-            const albumImg = document.createElement("img");
-            albumImg.src = img.src.medium;
-
-            // Aggiungi l'immagine e il titolo al div dell'album
-            albumDiv.appendChild(albumImg);
-            containerFoto.appendChild(albumDiv);
-
-        });
-    })
-  .catch(error => {
-    console.log('Si è verificato un errore:', error);
-  });
-
+              <div class="card-body">
+                
+                <div
+                  class="d-flex justify-content-between align-items-center"
+                >
+                  
+                  <small class="text-muted">${photo.id}</small>
+                </div>
+              </div>
+            </div> </div>`
+  }).join("")
+})
+.catch((err) => console.error(err))
 }
 
 
-
   let button = document.getElementById("search-button");
-
-  
 
   button.addEventListener("click", function(){
     let label = document.getElementById("inputSearch");
